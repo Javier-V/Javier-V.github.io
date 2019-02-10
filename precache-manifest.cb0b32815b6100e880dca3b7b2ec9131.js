bc47e59a010e048edd9a2fbc4916c5d1bc5f4f24
JV <javiervaleroenriquez@outlook.com>
2019-02-10 11:11:42 +0100

    initial commit for githubpages

---
+++ precache-manifest.cb0b32815b6100e880dca3b7b2ec9131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c772a71bf0b1606a30df",
+    "url": "/static/js/main.c772a71b.chunk.js"
+  },
+  {
+    "revision": "9a90f8a43667d4a538f3",
+    "url": "/static/js/1.9a90f8a4.chunk.js"
+  },
+  {
+    "revision": "c772a71bf0b1606a30df",
+    "url": "/static/css/main.593b4560.chunk.css"
+  },
+  {
+    "revision": "27947157bf6d8dc3bea06e38845655a1",
+    "url": "/index.html"
+  }
+];
